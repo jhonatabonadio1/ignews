02b35e59e3bfed8d7d5031cfc5fa0f06481a5de4
˜Jhonata <jhonbonadio@gmail.com>
2022-05-16 01:43:09 -0300

    adding inline-react-svg

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,13 +5,13 @@ import {
     NavLink
 } from './styles'
 
-import logo from '../../assets/images/logo.svg'
+import Logo from '../../assets/images/logo.svg'
 
 export function Header(){
     return(
         <HeaderContainer>
             <HeaderContent>
-                <img src={logo} />
+                <Logo />
                 <Nav>
                     <NavLink active>Home</NavLink>
                     <NavLink>Posts</NavLink>
